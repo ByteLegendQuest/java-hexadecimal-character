5994a805522859dfdb7af1fca82f71277c580e00
xxldm <zenglengceng@outlook.com>
2022-04-27 08:33:15 +0000

    Update at 2022-04-27T08:33:15.459565Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.util.regex.Pattern;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("A is valid hex character: " + isValidHexCharacter('A'));
@@ -13,6 +15,6 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        return false;
+        return Pattern.matches("\\d|[a-f|A-F]", Character.toString(ch));
     }
 }
