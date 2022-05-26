f83146761a9993873b61d31fc2d0d974545404b5
Yeely0162 <Yeely01621@163.com>
2022-05-26 00:47:38 +0000

    Update at 2022-05-26T00:47:38.498352Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,7 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        return false;
+        return ch <= '9' && ch >= '0' || 'A' <= ch && ch <= 'F' || 'a' <= ch && ch <= 'f';
+
     }
 }
