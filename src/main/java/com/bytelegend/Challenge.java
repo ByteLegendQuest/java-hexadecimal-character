d30d713b7085758e912f607aa56482c14d1372f2
amine01laaboudi <al2000229@gmail.com>
2022-07-18 19:08:30 +0000

    Update at 2022-07-18T19:08:30.882401Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        if (ch >= '0' && a <= '9' || ch >= 'A' && a <= 'Z' || ch >= 'a' && a <= 'z') {
+        if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
             return true;
         }
         return false;
