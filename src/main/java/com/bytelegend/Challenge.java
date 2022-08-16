f91d7f6514464dce45a2a5004cd1fcd2887d0a74
lpkyyds <2065870265@qq.com>
2022-08-16 05:59:21 +0000

    Update at 2022-08-16T05:59:21.010190Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,7 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        return false;
+        final String hexCharacter = "0123456789ABCDEF";
+        return hexCharacter.contains(String.valueOf(ch));
     }
 }
