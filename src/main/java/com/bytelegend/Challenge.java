01beec3eb24c102817a9dd3b445b3eb82111297f
agirnob <kakcayigitbaran@gmail.com>
2022-08-17 16:38:34 +0000

    Update at 2022-08-17T16:38:34.387119Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,9 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        return false;
+        char lower = Character.toLowerCase(ch);
+        if (Character.isDigit(ch) || lower >= 'a' && lower <= 'f') {
+            return true;
+        } else return false;
     }
 }
