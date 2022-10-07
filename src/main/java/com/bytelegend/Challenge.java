b16532c439a47c0d9066ccfcf0c6e1da6f125402
YuanHao97 <y1997h@126.com>
2022-10-07 13:14:59 +0000

    Update at 2022-10-07T13:14:59.696296Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,8 @@
 package com.bytelegend;
 
+import java.util.Locale;
+import java.util.regex.Pattern;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("A is valid hex character: " + isValidHexCharacter('A'));
@@ -13,6 +16,8 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        return false;
+        String pattern = "[\\d|a-f]";
+
+        return Pattern.matches(pattern, String.valueOf(ch).toLowerCase(Locale.ROOT));
     }
 }
