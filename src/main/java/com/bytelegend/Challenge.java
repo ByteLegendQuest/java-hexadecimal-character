aaf463b98740160c9a9d865c1851505db3d8e672
tiger0961 <29515547@qq.com>
2022-10-13 06:05:34 +0000

    Update at 2022-10-13T06:05:34.694370Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,8 +12,9 @@ public class Challenge {
      *
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
-    public static boolean isValidHexCharacter(String str) {
+    public static boolean isValidHexCharacter(char ch) {
         boolean flag = false;
+        String str = Character.toString(ch);
         for (int i = 0; i < str.length(); i++) {
             char cc = str.charAt(i);
             if (cc == '0' || cc == '1' || cc == '2' || cc == '3' || cc == '4' || cc == '5' || cc == '6' || cc == '7' || cc == '8' || cc == '9' || cc == 'A' || cc == 'B' || cc == 'C' ||
