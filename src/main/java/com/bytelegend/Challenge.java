5a283b1d50715a7e949a8b9a2a78456937e9c9da
guakun01 <guakun01@gmail.com>
2023-03-18 14:41:36 +0000

    Update at 2023-03-18T14:41:36.838065Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,17 +13,6 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        // 获取字符的ascii码
-        int ascii = (int) ch;
-        int zeroCode = 48;
-        int nineCode = 57;
-        int upperCaseACode = 65;
-        int upperCaseFCode = 70;
-        int lowerCaseACode = 97;
-        int lowerCaseFCode = 102;
-        // 如果ascii码在48-57之间，表示是数字0-9
-        // 如果ascii码在65-70之间，表示是大写字母A-F
-        // 如果ascii码在97-102之间，表示是小写字母a-f
-        return (ascii >= zeroCode && ascii <= nineCode) || (ascii >= upperCaseACode && ascii <= upperCaseFCode) || (ascii >= lowerCaseACode && ascii <= lowerCaseFCode);
+        return (ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F');
     }
 }
