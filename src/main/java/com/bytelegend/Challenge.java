e8fb196eebe8cffa37b2cb64bf61be909ee7c86b
Jungmin-Moon <jmmoon96@gmail.com>
2023-06-05 15:59:55 +0000

    Update at 2023-06-05T15:59:55.419249Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
      * <p>If the given character is a valid hex character, return `true`, otherwise return `false`.
      */
     public static boolean isValidHexCharacter(char ch) {
-        if ("abcdefABCDEFG0123456789".indexOf(ch) != -1) 
+        if ("abcdefABCDEF0123456789".indexOf(ch) != -1) 
             return true;
         return false;
     }
